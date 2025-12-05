372afd7096efcb8e2a2fbeb2ae80018baccbfe79
Phuc Nguyen <buidophucnguyen9721@gmail.com>
2025-12-05 16:10:47 +0700

    fix CORS

--- src/main/java/org/example/qlttngoaingu/config/WebSecurityConfig.java
+++ src/main/java/org/example/qlttngoaingu/config/WebSecurityConfig.java
@@ -64,7 +64,10 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001","https://quan-ly-trung-tam-ngoai-ngu.vercel.app/")); // Thêm các domain khác nếu cần
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"
+                , "http://localhost:3001"
+                ,"https://quan-ly-trung-tam-ngoai-ngu.vercel.app"
+                ,"https://quan-ly-trung-tam-ngoai-ngu-admin.vercel.app")); // Thêm các domain khác nếu cần
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 
